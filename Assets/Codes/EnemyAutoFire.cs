19e10d2bb2604dbbee2749ed68f914906c65a49e
jawad0felemban <JMfelemban@gmail.com>
2024-02-18 11:30:17 +0300

    make Enemy Shot

---
+++ Assets/Codes/EnemyAutoFire.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAutoFire : MonoBehaviour
+{
+    [SerializeField] private GameObject m_bulletstCG;
+    [SerializeField] private GameObject m_bulletstCB;
+    [SerializeField] private GameObject m_bulletstCX;
+    [SerializeField] private float m_FireRateOfShoting;
+
+    private Rigidbody m_bulletsE;
+
+
+    private void Awake()
+    {
+
+        m_bulletsE = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    private void Update()
+    {
+        InvokeRepeating("ShootB", 0.5f ,m_FireRateOfShoting);
+        //for (int i = 0; i == 4; i++)
+        //{
+        //    if (i == 0)
+        //    {
+        //        InvokeRepeating("ShootB", 0.5f, m_FireRateOfShoting);
+        //    }
+        //    if (i == 1)
+        //    {
+        //        InvokeRepeating("ShootG", 0.5f, m_FireRateOfShoting);
+        //    }
+        //    if (i == 2)
+        //    {
+        //        InvokeRepeating("ShootX", 0.5f, m_FireRateOfShoting);
+        //    }
+        //    else
+        //        i = 0;
+        //}
+    }
+
+    private void ShootB()
+    {
+        Instantiate(m_bulletstCB, transform.position, Quaternion.identity);
+    }
+    private void ShootG()
+    {
+        Instantiate(m_bulletstCG, transform.position, Quaternion.identity);
+    }
+    private void ShootX()
+    {
+        Instantiate(m_bulletstCX, transform.position, Quaternion.identity);
+    }
+}
